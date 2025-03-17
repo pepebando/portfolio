83be949c58f88a1b284d71b98168ef33e304d0b6
Pepe Obando <jsobando96@hotmail.com>
2025-03-17 08:04:17 -0500

    te

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="flex h-screen w-screen bg-black text-white overflow-hidden">
     <main class="w-full">
       <div class="w-full flex h-90/100">
-
+        
         <!-- Sidebar Desktop (Solo en Desktop) -->
         <div class="w-2/10 p-4 m-2 bg-black flex flex-col rounded-lg scrollbar overflow-y-scroll hidden lg:block" >
           <div @click="PerfilActual = 'Home'" class="flex items-center space-x-2 mb-4 cursor-pointer transition-opacity">
@@ -24,74 +24,86 @@
             </ul>
           </div>
         </div>
-
+        
         <!-- Botón de Menú Móvil (Solo en móvil) -->
-        <button @click="menuOpen = true" class="fixed top-4 left-4 z-50 p-2 bg-gray-800 text-white rounded-md block lg:hidden">
-          <MenuIcon class="w-6 h-6" />
+        <button @click="menuOpen = true" class="fixed top-4 right-5 z-50 p-2 bg-gray-800 text-white rounded-md block lg:hidden">
+          <MenuIcon class="w-12 h-12" />
         </button>
-
+        
         <!-- Menú Responsive en Móvil -->
         <Transition name="slide">
-          <div v-if="menuOpen" class="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md z-50 flex"
-               @click="menuOpen = false"
-               @touchstart="menuOpen = false">
-
-            <!-- Contenedor del Menú -->
-            <div class="w-72 bg-black text-white p-6 shadow-lg z-50 relative" @click.stop>
-              <button @click="menuOpen = false" class="absolute top-4 right-4 text-white">
-                ✖
-              </button>
-
-              <div class="flex items-center space-x-3 mb-6">
-                <img src="https://via.placeholder.com/50" class="w-12 h-12 rounded-full" />
-                <div>
-                  <h2 class="text-lg font-bold">Pepe Obando</h2>
-                  <p class="text-gray-400 text-sm">Ver perfil</p>
-                </div>
-              </div>
-              
-              <nav class="space-y-4">
-                <ul>
-                  <SidebarCard v-for="(project, index) in sidebararray" :key="index" :title="project.title" :image="project.image" />
-                </ul>
-              </nav>
-            </div>
-
+          <div v-if="menuOpen" class="fixed inset-0  bg-black bg-opacity-50 backdrop-blur-md z-50 flex"
+          @click="menuOpen = false"
+          @touchstart="menuOpen = false">
+          
+          <!-- Contenedor del Menú -->
+          
+          <div class="f-right w-full bg-black text-white p-6 shadow-lg z-50 relative p-15" @click.stop>
+            <button @click="menuOpen = false" class="absolute top-4 left-4 text-white w-12 h-12 z-999">
+              <XIcon class="w-12 h-12" />
+            </button>  
+            <nav class="space-y-4">
+              <ul>
+                <li class="p-5">
+                  <div @click="PerfilActual = 'Home'" class="flex justify-center items-center gap-x-4 rounded-xl pt-3 pb-3 pl-1 border-1 bg-black hover:bg-greyspotclear">
+                    <HomeIcon class="w-12 h-12" />
+                    <span>Home</span>
+                  </div>
+                </li>
+                <li class="p-5">
+                  <div @click="PerfilActual = 'Projects'" class="flex justify-center items-center gap-x-4 rounded-xl pt-3 pb-3 pl-1 border-1 bg-black hover:bg-greyspotclear">
+                    <LibraryIcon class="w-12 h-12" />
+                    <span>All my projects</span>
+                  </div>
+                </li>
+                <li class="p-5">
+                  <div @click="PerfilActual = 'Contact'" class="flex justify-center items-center gap-x-4 rounded-xl pt-3 pb-3 pl-1 border-1 bg-black hover:bg-greyspotclear">
+                    <MailIcon class="w-12 h-12" />
+                    <span>Contact</span>
+                  </div>
+                </li>
+                <SidebarResponsive  v-for="(project, index) in sidebararray" :key="index" :title="project.title" :image="project.image" />
+              </ul>
+            </nav>
           </div>
-        </Transition>
-
-        <!-- Transición entre Secciones -->
-        <Transition name="fade" mode="out-in">
-          <component :is="currentComponent" :key="PerfilActual" />
-        </Transition>
-      </div>
+          
+        </div>
+      </Transition>
       
-      <!-- Barra de Reproducción -->
-      <PlayBar class="fixed" />
-      
-    </main>
-  </div>
+      <!-- Transición entre Secciones -->
+      <Transition name="fade" mode="out-in">
+        <component :is="currentComponent" :key="PerfilActual" />
+      </Transition>
+    </div>
+    
+    <!-- Barra de Reproducción -->
+    <PlayBar class="fixed" />
+    
+  </main>
+</div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
+import SidebarResponsive from '/src/components/SidebarResponsive.vue';
 import SidebarCard from '/src/components/SidebarCard.vue';
 import PortfolioUI from '/src/components/PortfolioUI.vue';
 import PerfilUI from '/src/components/PerfilUI.vue';
 import PlayBar from '/src/components/PlayBar.vue';
 import ContactForm from '/src/components/ContactForm.vue';
-import { HomeIcon, LibraryIcon, MailIcon, MenuIcon } from 'lucide-vue-next';
+
+import { HomeIcon, LibraryIcon, MailIcon, MenuIcon,XIcon } from 'lucide-vue-next';
 
 const sidebararray = ref([
-  { title: 'Unreal Engine', image: "https://img.icons8.com/nolan/240/unreal-engine.png" },
-  { title: 'Web', image: "https://img.icons8.com/cute-clipart/64/web.png" },
-  { title: 'Augmented Reality', image: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-augmented-reality-new-media-flaticons-lineal-color-flat-icons-2.png" },
-  { title: 'Mixed Reality', image: "https://cdn.brandfetch.io/idWvz5T3V7/w/400/h/400/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B" },
-  { title: 'Virtual Reality', image: "https://img.icons8.com/plasticine/100/virtual-reality.png" },
-  { title: 'Videogames', image: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-gaming-live-streaming-flaticons-flat-flat-icons.png" },
-  { title: 'Python', image: "https://cdn.brandfetch.io/idbpOFBgcc/w/398/h/398/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B" },
-  { title: 'APPs', image: "https://img.icons8.com/color/48/apple-app-store--v3.png" },
-  { title: 'Others', image: "https://cdn.brandfetch.io/id93sjCX4f/w/300/h/300/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B" }
+{ title: 'Unreal Engine', image: "https://img.icons8.com/nolan/240/unreal-engine.png" },
+{ title: 'Web', image: "https://img.icons8.com/cute-clipart/64/web.png" },
+{ title: 'Augmented Reality', image: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-augmented-reality-new-media-flaticons-lineal-color-flat-icons-2.png" },
+{ title: 'Mixed Reality', image: "https://cdn.brandfetch.io/idWvz5T3V7/w/400/h/400/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B" },
+{ title: 'Virtual Reality', image: "https://img.icons8.com/plasticine/100/virtual-reality.png" },
+{ title: 'Videogames', image: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-gaming-live-streaming-flaticons-flat-flat-icons.png" },
+{ title: 'Python', image: "https://cdn.brandfetch.io/idbpOFBgcc/w/398/h/398/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B" },
+{ title: 'APPs', image: "https://img.icons8.com/color/48/apple-app-store--v3.png" },
+{ title: 'Others', image: "https://cdn.brandfetch.io/id93sjCX4f/w/300/h/300/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B" }
 ]);
 
 const PerfilActual = ref("Home");
@@ -126,11 +138,11 @@ const currentComponent = computed(() => {
   transition: transform 0.3s ease, opacity 0.3s ease;
 }
 .slide-enter-from {
-  transform: translateX(-100%);
+  transform: translateX(100%);
   opacity: 0;
 }
 .slide-leave-to {
-  transform: translateX(-100%);
+  transform: translateX(100%);
   opacity: 0;
 }
 </style>
---
+++ src/components/SideBarResponsive.vue
@@ -0,0 +1,13 @@
+<template>
+    <li class="cursor-pointer p-5">            
+        <div class=" flex justify-center items-center gap-x-4 rounded-xl pt-3 pb-3 pl-1 border-1 bg-black hover:bg-greyspotclear ">
+            <img :src="image" class="size-12 shrink-0"  alt="Logo" />
+            <div>
+                <div class="text-xl font-medium text-black text-white " >{{ title }}</div>
+            </div>
+        </div>
+    </li>
+</template>
+<script setup lang="ts">
+defineProps<{ title: string, image: string }>();
+</script>
