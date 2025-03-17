aadb9316192233279b76eb49e8b3f5462a75052b
Pepe Obando <jsobando96@hotmail.com>
2025-03-17 09:29:35 -0500

    test

--- src/components/CertificatesCards.vue
+++ src/components/CertificatesCards.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="flex items-center justify-between py-2 border-b border-gray-700">
         <div class="flex items-center space-x-4">
-            <span class="text-gray-400">{{ number }}</span>
+            <span class="text-xl lg:text-sm text-gray-400">{{ number }}</span>
             
-            <img :src="image" class="w-20 h-20 rounded-full object-cover" alt="SocialMedia" />
+            <img :src="image" class="w-30 h-30 lg:w-20 lg:h-20 rounded-full object-cover" alt="SocialMedia" />
             <div>
-                <p class="font-bold">{{ title }}</p>
-                <p class="text-sm text-gray-400">{{ description }}</p>
+                <p class="text-2xl lg:text-sm font-bold">{{ title }}</p>
+                <p class="text-2xl lg:text-sm text-gray-400">{{ description }}</p>
             </div>
         </div>
-        <span class="text-gray-400">{{ duration }}</span>
+        <span class="text-xl lg:text-sm text-gray-400">{{ duration }}</span>
     </div>
 </template>
 
--- src/components/PerfilCardUI.vue
+++ src/components/PerfilCardUI.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mb-10 ">
       <img :src="image" class="w-40 h-40 lg:w-25 lg:h-25 rounded-full object-cover mb-5" alt="Artist Image" />
-      <h1 class="text-4xl mt-2 text-sm text-center" >{{ title }}</h1>
+      <h1 class="text-2xl mt-2 lg:text-sm text-center" >{{ title }}</h1>
     </div>
   </template>
   
